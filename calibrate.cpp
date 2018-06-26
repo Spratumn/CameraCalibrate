772579e94f26da9750eb2add8ad675293ecf3c39
Spratumn <32872875+Spratumn@users.noreply.github.com>
2018-06-26 22:52:30 +0800

    Update calibrate.cpp

--- calibrate.cpp
+++ calibrate.cpp
@@ -5,21 +5,21 @@ int main()
 
     Mat image;
     vector<string> filelist;
-    for (int i=21; i<=30; i++)
+    for (int i=1; i<=4; i++)
     {
         stringstream str;
-        str << "../chessboards/chessboard" << std::setw(2) << std::setfill('0') << i << ".jpg";
+        str << "../chessboard/left" << std::setw(2) << std::setfill('0') << i << ".jpg";
         cout << str.str() << endl;
         filelist.push_back(str.str());
         image= imread(str.str());
         imshow("Image",image);
         waitKey(100);
     }
-    Size size(6,4);
+    Size size(9,6);
     calibrator calib(filelist,size,10);
     calib.calibrate();
     cout<<"cameraMatrix:"<<calib.getDistCoeffs()<<endl;
-    Mat image1=imread("../chessboards/chessboard14.jpg",0);
+    Mat image1=imread("../chessboard/left02.jpg",0);
     Mat image2= calib.rectifyImage(image1);
     imshow("orginal image",image1);
     imshow("rectified image",image2);
